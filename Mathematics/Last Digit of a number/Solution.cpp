364ab36332785db538262b243d556d623a27679b
Md Imran <187121726+mdimran2211@users.noreply.github.com>
2026-08-15 00:56:20 +0530

    feat: add GFG solution - Last Digit of a number

---
+++ Mathematics/Last Digit of a number/Solution.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    // code here
+    int c=abs(n)%10;
+    cout << c;
+
+    return 0;
+}
