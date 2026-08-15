20dbc493b61db474488b21dbe5796e00834e54ae
Md Imran <187121726+mdimran2211@users.noreply.github.com>
2026-08-15 23:13:21 +0530

    feat: add GFG solution - class Solution:    def lcmAndGcd(self, a : int, b : int) -> List[int]:        # code here        L1,T2=a,b        while b>0:            r=a%b            a=b            b=r        gcd=a        lcm=int((L1*T2)/gcd)        return lcm,gcd

---
+++ Mathematics/class Solution  def lcmAndGcdself a  int b  int - Listint     code here    L1T2ab    while b0      rab      ab      br    gcda    lcmintL1T2gcd    return lcmgcd/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static int[] lcmAndGcd(int a, int b) {
+        // code here
+        int x = a;
+        int y = b;
+       
+        while (y != 0) {
+            int temp = y;
+            y = x % y;
+            x = temp;
+        }
+        int gcd = x;
+        
+        int lcm = (a * b) / gcd;
+        return new int[]{lcm, gcd};
+    }
+}
