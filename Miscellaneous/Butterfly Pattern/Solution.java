47ddf997464c11dda6fb384f5fa531e4395a5d12
Md Imran <187121726+mdimran2211@users.noreply.github.com>
2026-08-16 22:46:06 +0530

    feat: add GFG solution - Butterfly Pattern

---
+++ Miscellaneous/Butterfly Pattern/Solution.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // Upper half
+        for(int i = 1; i <= n; i++) {
+
+            if(i == n) {
+                for(int j = 1; j <= 2 * n - 1; j++) {
+                    System.out.print("*");
+                }
+            } 
+            else {
+                for(int j = 1; j <= i; j++) {
+                    System.out.print("*");
+                }
+
+                int spaces = 2 * (n - i) - 1;
+
+                for(int j = 1; j <= spaces; j++) {
+                    System.out.print(" ");
+                }
+
+                for(int j = 1; j <= i; j++) {
+                    System.out.print("*");
+                }
+            }
+
+            System.out.println();
+        }
+
+        // Lower half
+        for(int i = n - 1; i >= 1; i--) {
+
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
