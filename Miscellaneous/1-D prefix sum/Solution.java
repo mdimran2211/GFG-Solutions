d2e26ecf35acfa2d7d41e457ddb1ad5da9bf82bb
Md Imran <187121726+mdimran2211@users.noreply.github.com>
2026-08-30 22:06:16 +0530

    feat: add GFG solution - 1-D prefix sum

---
+++ Miscellaneous/1-D prefix sum/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public ArrayList<Integer> prefSum(int[] arr) {
+        // code here
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        int sum =0;
+        for(int i =0; i<arr.length; i++){
+            sum+= arr[i];
+            result.add(sum);
+        }
+        return result;
+    }
+}
